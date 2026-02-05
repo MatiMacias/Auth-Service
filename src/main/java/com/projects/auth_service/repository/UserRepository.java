dc6f9992449de707353240e899d4f619be02c6a2
Matias Macias <matiaspitumacias25@gmail.com>
2026-02-05 17:45:35 -0300

    dockerizando DB y cambio a application.yml

--- src/main/java/com/projects/auth_service/repository/UserRepository.java
+++ src/main/java/com/projects/auth_service/repository/UserRepository.java
@@ -7,6 +7,6 @@ import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    Optional<User> findByUsername(String username);
-    boolean  existByUsername(String username);
+    /*Optional<User> findByUsername(String username);
+    boolean existByUsername(String username);*/
 }
