0dde75fd7dbd23d499a67de352ee286a2cd46219
Matias Macias <matiaspitumacias25@gmail.com>
2026-02-06 15:27:52 -0300

    controller basico y endpoint de prueba

--- src/main/java/com/projects/auth_service/controller/AuthController.java
+++ src/main/java/com/projects/auth_service/controller/AuthController.java
@@ -1,4 +1,24 @@
 package com.projects.auth_service.controller;
 
+import com.projects.auth_service.dto.AuthResponse;
+import com.projects.auth_service.dto.LoginRequest;
+import com.projects.auth_service.dto.RegisterRequest;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/auth")
 public class AuthController {
+
+    @PostMapping("/register")
+    public ResponseEntity<String> register(@RequestBody RegisterRequest request){
+        return ResponseEntity.ok("dummy-token");
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody LoginRequest request){
+        return ResponseEntity.ok("dummy-token");
+    }
+
 }
