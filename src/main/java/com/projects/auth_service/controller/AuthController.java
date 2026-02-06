25129a15b252f7c9343f350695f782e819e396a6
Matias Macias <matiaspitumacias25@gmail.com>
2026-02-06 15:45:20 -0300

    service temporal y passwordEncoder

--- src/main/java/com/projects/auth_service/controller/AuthController.java
+++ src/main/java/com/projects/auth_service/controller/AuthController.java
@@ -3,6 +3,7 @@ package com.projects.auth_service.controller;
 import com.projects.auth_service.dto.AuthResponse;
 import com.projects.auth_service.dto.LoginRequest;
 import com.projects.auth_service.dto.RegisterRequest;
+import com.projects.auth_service.service.AuthService;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,14 +12,20 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/auth")
 public class AuthController {
 
+    private final AuthService authService;
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
     @PostMapping("/register")
     public ResponseEntity<String> register(@RequestBody RegisterRequest request){
-        return ResponseEntity.ok("dummy-token");
+        return ResponseEntity.ok(authService.register(request.getEmail(), request.getPassword()));
     }
 
     @PostMapping("/login")
     public ResponseEntity<String> login(@RequestBody LoginRequest request){
-        return ResponseEntity.ok("dummy-token");
+        return ResponseEntity.ok(authService.login(request.getUsername(), request.getPassword()));
     }
 
 }
